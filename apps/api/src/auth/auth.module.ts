9625209a7f33fea1db09b5dfad3ab656153c6231
Anderson Rocha <andersonrocha.rs@live.com>
2026-03-03 19:35:46 -0300

    feat(auth): configure jwt module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
   providers: [AuthResolver, AuthService],
   exports: [AuthService],
 })
