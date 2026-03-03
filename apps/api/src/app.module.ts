be6d037fdf3cdf4f32e3f78750df393ef9aad888
Anderson Rocha <andersonrocha.rs@live.com>
2026-03-03 19:34:34 -0300

    feat(auth): create auth module structure

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     PrismaModule,
+    AuthModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
